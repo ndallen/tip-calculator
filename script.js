ea2bd96708f76664ba96b54889c604b322dccdc5
ndallen <ecommerceevolution@gmail.com>
2019-02-01 10:10:20 +0700

    created DOM output variables and rounded

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.getElementById('container').onchange = function() {
-  var bill = document.getElementById('billTotal').value;
+  var bill = Number(document.getElementById('billTotal').value);
   var tipPercent = document.getElementById('tipInput').value;
   var splitNumberPeople = document.getElementById('splitInput').value;
+  var tipValue = Math.round(bill * (tipPercent / 100));
+  var newBillEach = Math.round((bill + tipValue) / splitNumberPeople);
+
+  document.getElementById('tipOutput').innerHTML = tipPercent + '%';
+  document.getElementById('splitOutput').innerHTML = splitNumberPeople;
+  document.getElementById('newBill').innerHTML = '$' + newBillEach;
+  document.getElementById('tip').innerHTML ='$' + tipValue / splitNumberPeople;
 }
