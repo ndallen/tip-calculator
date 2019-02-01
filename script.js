34352bdaa636cb764ab470d8316a4b46e7c48b82
ndallen <ecommerceevolution@gmail.com>
2019-02-01 09:21:55 +0700

    created DOM javascript function to catch input data

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('container').onchange = function() {
+  var bill = document.getElementById('billTotal').value;
+  var tipPercent = document.getElementById('tipInput').value;
+  var splitNumberPeople = document.getElementById('splitInput').value;
+}
